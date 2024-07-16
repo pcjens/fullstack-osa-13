4a225adf42caacba9eac799c0644039a67f35188
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-07-16 19:34:28 +0300

    Add error handling (13.7)

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const dotenv = require('dotenv');
-const { Sequelize } = require('sequelize')
 const express = require('express');
 const { Blog } = require('./models');
 
 const app = express()
 dotenv.config();
-const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 app.use(express.json());
 
@@ -14,38 +12,47 @@ app.get('/api/blogs', async (req, res) => {
     res.json(blogs);
 });
 
-app.post('/api/blogs', async (req, res) => {
+app.post('/api/blogs', async (req, res, next) => {
     try {
         console.log(req.body);
         const blog = await Blog.create(req.body);
         res.json(blog);
     } catch (error) {
-        return res.status(500).json({ error });
+        next(error);
     }
 });
 
 const blogFinder = async (req, res, next) => {
-    req.blog = await Blog.findByPk(req.params.id);
-    next();
+    try {
+        req.blog = await Blog.findByPk(req.params.id);
+        next();
+    } catch (error) {
+        next(error);
+    }
 };
 
-app.delete('/api/blogs/:id', blogFinder, async (req, res) => {
+app.delete('/api/blogs/:id', blogFinder, async (req, res, next) => {
     try {
         await req.blog.destroy();
         res.sendStatus(200);
     } catch (error) {
-        return res.status(500).json({ error });
+        next(error);
     }
 });
 
-app.put('/api/blogs/:id', blogFinder, async (req, res) => {
+app.put('/api/blogs/:id', blogFinder, async (req, res, next) => {
     try {
         req.blog.likes = req.body.likes;
         await req.blog.save();
         res.sendStatus(200);
     } catch (error) {
-        return res.status(500).json({ error });
+        next(error);
     }
+});
+
+app.use((error, req, res, next) => {
+    console.error('Error handling request: ' + error.message);
+    res.sendStatus(500);
 });
 
 const PORT = process.env.PORT || 3001;
