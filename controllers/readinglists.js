99e04f35af5f20e1e3a11db9db4da14b494b65a3
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-07-17 20:27:58 +0300

    Add endpoint for marking reading list entries as read (13.22)

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { Op } = require('sequelize');
-const { User, Blog, Reading } = require('../models');
-const { sequelize } = require('../util/db');
+const { Reading } = require('../models');
+const { loggedInUserSetter } = require('../util/auth');
 
 const router = express.Router();
 
@@ -10,6 +9,19 @@ router.post('/', async (req, res, next) => {
         const { blog_id: blogId, user_id: userId } = req.body;
         const newReading = await Reading.create({ read: false, userId, blogId });
         res.json(newReading);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/:id', loggedInUserSetter, async (req, res, next) => {
+    try {
+        const reading = await Reading.findByPk(req.params.id);
+        if (reading.userId !== req.loggedInUser.id)
+            return res.status(401).json({ error: 'only the owner of the reading entry can change its read status' });
+        reading.read = req.body.read;
+        await reading.save();
+        res.json(reading);
     } catch (error) {
         next(error);
     }
